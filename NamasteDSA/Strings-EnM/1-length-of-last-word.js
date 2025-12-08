64e704e185b37f43d954b73437363c513d809994
Vikash Kumar <vikashkumar995533@gmail.com>
2025-12-08 21:39:11 +0530

    Implement lengthOfLastWord function to return the length of the last word in a string

---
+++ NamasteDSA/Strings-EnM/1-length-of-last-word.js
@@ -0,0 +1,33 @@
+/**
+ * Given a string s consisting of words and spaces, 
+ * return the length of the last word in the string.
+ * A word is a maximal substring consisting of non-space characters only.
+ */
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    // Solution 1: Using JS in-built functions
+    // const splitted = s.trim().split(' ');
+    // const last = splitted.at(splitted.length - 1);
+    // return last.length;
+
+    // Solution 2: Using for-loop
+    let temp = '';
+    let isLastStr = true;
+    for (const char of s) {
+        if (char === ' ') {
+            isLastStr = false;
+            continue;
+        }
+        if (!isLastStr) {
+            isLastStr = true;
+            temp = '';
+        }
+        temp += char;
+    }
+
+    return temp.length;
+};
