699139bf6927678b3c2ae3e90dedf18f53f17db2
Vikash Kumar <vikashkumar995533@gmail.com>
2025-12-09 23:45:35 +0530

    Refactor lengthOfLastWord function to remove unused code and improve readability

--- NamasteDSA/Strings-EnM/1-length-of-last-word.js
+++ NamasteDSA/Strings-EnM/1-length-of-last-word.js
@@ -8,26 +8,40 @@
  * @param {string} s
  * @return {number}
  */
-var lengthOfLastWord = function(s) {
+var lengthOfLastWord = function (s) {
     // Solution 1: Using JS in-built functions
     // const splitted = s.trim().split(' ');
     // const last = splitted.at(splitted.length - 1);
     // return last.length;
 
     // Solution 2: Using for-loop
-    let temp = '';
-    let isLastStr = true;
-    for (const char of s) {
-        if (char === ' ') {
-            isLastStr = false;
-            continue;
+    // let temp = '';
+    // let isLastStr = true;
+    // for (const char of s) {
+    //     if (char === ' ') {
+    //         isLastStr = false;
+    //         continue;
+    //     }
+    //     if (!isLastStr) {
+    //         isLastStr = true;
+    //         temp = '';
+    //     }
+    //     temp += char;
+    // }
+
+    // return temp.length;
+
+    // Solution 3: Use one loop from last
+    let n = s.length - 1;
+    let count = 0;
+    while (n >= 0) {
+        if (s[n] !== ' ') {
+            count++;
+        } else if (count > 0) {
+            break;
         }
-        if (!isLastStr) {
-            isLastStr = true;
-            temp = '';
-        }
-        temp += char;
+        n--;
     }
 
-    return temp.length;
+    return count;
 };
