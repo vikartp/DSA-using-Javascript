0e637ec6e142c0909445ed41771c05408ce032c6
Vikash Kumar <vikashkumar995533@gmail.com>
2025-12-18 20:49:50 +0530

    Refactor isPalindrome function to use two-pointer technique for improved efficiency

--- NamasteDSA/Strings-EnM/7-valid-palindrome.js
+++ NamasteDSA/Strings-EnM/7-valid-palindrome.js
@@ -16,15 +16,35 @@ var isPalindrome = function (s) {
     // return cleaned === reversed;
 
     // Approach 3:
-    s = s.toLowerCase();
-    let filtered = '';
-    for (const char of s) {
-        if (char.match(/[a-zA-Z0-9]/g)) {
-            filtered += char;
-        }
-    }
-    const reversed = filtered.split('').reverse().join('');
-    return filtered === reversed
+    // s = s.toLowerCase();
+    // let filtered = '';
+    // for (const char of s) {
+    //     if (char.match(/[a-zA-Z0-9]/g)) {
+    //         filtered += char;
+    //     }
+    // }
+    // const reversed = filtered.split('').reverse().join('');
+    // return filtered === reversed
 
     // All three approaches above has Time Complexity of O(n) and Space Complexity of O(n)
+
+    // Use two pointer, Time: O(n), Space(1)
+    s = s.toLowerCase();
+    let left = 0;
+    let right = s.length - 1;
+
+    while (left < right) {
+        if (!s[left].match(/[a-z0-9]/g)) {
+            left++;
+            continue;
+        }
+        if (!s[right].match(/[a-z0-9]/g)) {
+            right--;
+            continue;
+        }
+        if (s[left] !== s[right]) return false;
+        left++;
+        right--;
+    }
+    return true;
 };
