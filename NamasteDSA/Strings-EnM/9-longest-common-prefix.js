1b45bca0af7790b98605af5a7ad2e405fe7c1dd7
Vikash Kumar <vikashkumar995533@gmail.com>
2025-12-30 22:00:56 +0530

    longestCommonPrefix

---
+++ NamasteDSA/Strings-EnM/9-longest-common-prefix.js
@@ -0,0 +1,34 @@
+/**
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ * If there is no common prefix, return an empty string "".
+ */
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+    if (!strs.length) return "";
+
+    const first = strs[0];
+
+    for (let i = 0; i < first.length; i++) {
+        const char = first[i];
+
+        for (let j = 1; j < strs.length; j++) {
+            if (i >= strs[j].length || strs[j][i] !== char) {
+                return first.slice(0, i);
+            }
+        }
+    }
+
+    return first;
+};
+
+// Time: O(N × M)
+
+// N = number of strings
+
+// M = length of the first string
+
+// Space: O(1) extra space
