31787eb4b548f8edaaa728ac5e550c4ffdb08b75
Vikash Kumar <vikashkumar995533@gmail.com>
2026-01-09 22:12:39 +0530

    Refactor searchRange function to improve left and right index finding logic using binary search

--- NamasteDSA/BinarySearchAlgo/7-find-first-and-last-position-of-element-in-sorted-array.js
+++ NamasteDSA/BinarySearchAlgo/7-find-first-and-last-position-of-element-in-sorted-array.js
@@ -33,30 +33,62 @@ var searchRange = function (nums, target) {
 
     // Solution 2: Find left and right index separately in two search iteration
     // Find left index
+    // let l = 0;
+    // let r = nums.length - 1;
+    // const ans = [-1, -1];
+    // while (l < r) {
+    //     const m = l + Math.floor((r - l) / 2);
+    //     if (target > nums[m]) {
+    //         l = m + 1;
+    //     } else {
+    //         r = m;
+    //     }
+    // }
+    // if (target === nums[l]) ans[0] = l;
+    // // Find right index
+    // l = 0;
+    // r = nums.length - 1;
+    // while (l < r) {
+    //     const m = l + Math.ceil((r - l) / 2); // Note: using ceil to avoid infinite loop
+    //     if (target < nums[m]) {
+    //         r = m - 1;
+    //     } else {
+    //         l = m;
+    //     }
+    // }
+    // if (target === nums[l]) ans[1] = l;
+
+    // return ans;
+
+    // Solution 3: Find the target and keep going with the loop- once left and once write
+    const ans = [-1, -1];
+
     let l = 0;
     let r = nums.length - 1;
-    const ans = [-1, -1];
-    while (l < r) {
+    while (l <= r) {
         const m = l + Math.floor((r - l) / 2);
-        if (target > nums[m]) {
+        if (nums[m] === target) {
+            ans[0] = m;
+            r = m - 1; // We are finding left side index, so we move 'r' towards left
+        } else if (target > nums[m]) {
             l = m + 1;
         } else {
-            r = m;
+            r = m - 1;
         }
     }
-    if (target === nums[l]) ans[0] = l;
-    // Find right index
+
     l = 0;
     r = nums.length - 1;
-    while (l < r) {
-        const m = l + Math.ceil((r - l) / 2); // Note: using ceil to avoid infinite loop
-        if (target < nums[m]) {
-            r = m - 1;
+    while (l <= r) {
+        const m = l + Math.floor((r - l) / 2);
+        if (nums[m] === target) {
+            ans[1] = m;
+            l = m + 1; // We are now finding right side index, so we move 'l' towards right
+        } else if (target > nums[m]) {
+            l = m + 1;
         } else {
-            l = m;
+            r = m - 1;
         }
     }
-    if (target === nums[l]) ans[1] = l;
-
     return ans;
 };
