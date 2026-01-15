0ba4bb045d17105c68a3b42de7be8e32378e0cd5
Vikash Kumar <vikashkumar995533@gmail.com>
2026-01-15 20:34:31 +0530

    maxArea

---
+++ NamasteDSA/TwoPointers/5-container-with-most-water.js
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/container-with-most-water/description/
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function (height) {
+    let max = 0;
+    let i = 0;
+    let j = height.length - 1;
+    while (i < j) {
+        const area = (j - i) * Math.min(height[i], height[j]);
+        max = Math.max(area, max);
+        height[i] <= height[j] ? i++ : j--;
+    }
+    return max;
+};
