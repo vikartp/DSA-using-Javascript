4281793094275b8b05e71d21ddb00adf5ab8cabe
Vikash Kumar <vikashkumar995533@gmail.com>
2026-04-05 15:12:00 +0530

    Add detailed comments to isValidBST function explaining top-down and bottom-up approaches

--- NamasteDSA/BinarySerachTree/validate-binary-search-tree.js
+++ NamasteDSA/BinarySerachTree/validate-binary-search-tree.js
@@ -13,6 +13,13 @@
  * @return {boolean}
  */
 var isValidBST = function(root) {
+    /**
+     * Solution 1: Top Down approach
+     * For each node, we will keep track of the low and high values which are the constraints for the current node's value. 
+     * Initially, for the root node, there are no constraints, so low and high will be null. 
+     * When we go to the left child, the high will be current node's value because all the values in left subtree should be less than current node's value. 
+     * When we go to the right child, the low will be current node's value because all the values in right subtree should be greater than current node's value.
+     */
     let ans = true;
     const traverse = (curr, low, high) => {
         // If low is not null and current node's value is less than or equal to low, then it's not a valid BST
@@ -31,6 +38,20 @@ var isValidBST = function(root) {
     // In the beginning, low and high will be null because there is no constraint on the root node
     traverse(root, null, null);
     return ans;
+
+    // Solution 2: Bottom Up Approach
+    // const isBST = (curr, l, h) => {
+    //     if (!curr) return true;
+    //     if ((l !== null && curr.val <= l) || (h !== null && curr.val >= h)) {
+    //         return false;
+    //     }
+    //     const isLeftBST = isBST(curr.left, l, curr.val);
+    //     const isRightBST = isBST(curr.right, curr.val, h);
+    //     // If both left and right subtree are BST, then current subtree is also BST
+    //     return isLeftBST && isRightBST;
+    // }
+
+    // return isBST(root, null, null);
 };
 
 // Time Complexity: O(n) where n is the number of nodes in the tree
