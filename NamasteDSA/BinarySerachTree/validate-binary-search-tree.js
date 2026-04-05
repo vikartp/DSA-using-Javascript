cabf568700f4805a5f62b208ff3ffd5b6a8cb2ee
Vikash Kumar <vikashkumar995533@gmail.com>
2026-04-05 14:48:12 +0530

    Add isValidBST function to validate a binary search tree

---
+++ NamasteDSA/BinarySerachTree/validate-binary-search-tree.js
@@ -0,0 +1,38 @@
+// https://leetcode.com/problems/validate-binary-search-tree/description/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+    let ans = true;
+    const traverse = (curr, low, high) => {
+        // If low is not null and current node's value is less than or equal to low, then it's not a valid BST
+        if (low !== null && curr.val <= low) {
+            ans = false;
+        }
+        // If high is not null and current node's value is greater than or equal to high, then it's not a valid BST
+        if (high !== null && curr.val >= high) {
+            ans = false;
+        }
+        // When we go to left child, the high will be current node's value because all the values in left subtree should be less than current node's value
+        curr.left && traverse(curr.left, low, curr.val);
+        // When we go to right child, the low will be current node's value because all the values in right subtree should be greater than current node's value
+        curr.right && traverse(curr.right, curr.val, high);
+    }
+    // In the beginning, low and high will be null because there is no constraint on the root node
+    traverse(root, null, null);
+    return ans;
+};
+
+// Time Complexity: O(n) where n is the number of nodes in the tree
+// Space Complexity: O(h) where h is the height of the tree, due to recursive stack space.
+// In worst case (skewed tree), it can be O(n).
