c5d4d2120fa6c01c985a77a82e3b649766c79725
Vikash Kumar <vikashkumar995533@gmail.com>
2026-06-17 17:09:51 +0530

    feat: implement coin change problem using top-down dynamic programming approach

---
+++ NamasteDSA/DP/coin-change.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function (coins, amount) {
+    // Solution 1: top down- recursive solution
+    const dp = {};
+    // const dp = new Map();
+
+    const fn = (rem) => {
+        if (dp[rem]) return dp[rem];
+        // if (dp.has(rem)) {
+        //     return dp.get(rem);
+        // }
+        if (rem === 0) return 0;
+        if (rem < 0) return -1;
+
+        let minCoins = Infinity;
+        for (const c of coins) {
+            const res = fn(rem - c);
+            if (res !== -1) {
+                minCoins = Math.min(minCoins, 1 + res);
+            }
+        }
+
+        dp[rem] = minCoins !== Infinity ? minCoins : -1;
+        // dp.set(rem, minCoins !== Infinity ? minCoins: -1)
+        return dp[rem];
+        // return dp.get(rem);
+    }
+
+    return fn(amount);
+};
